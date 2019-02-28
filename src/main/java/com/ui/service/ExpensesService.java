b5ce1308273404a00a974bd6954ce31c2d84446e
evgenyandroschuk <johnstep70483@gmail.com>
2019-02-28 21:29:07 +0300

    [CF] Changing name of month

--- src/main/java/com/ui/service/ExpensesService.java
+++ src/main/java/com/ui/service/ExpensesService.java
@@ -349,7 +349,7 @@ public class ExpensesService extends AbstractService {
             case 2 : month = "Февраль"; break;
             case 3 : month = "Март"; break;
             case 4 : month = "Апрель"; break;
-            case 5 : month = "Март"; break;
+            case 5 : month = "Май"; break;
             case 6 : month = "Июнь"; break;
             case 7 : month = "Июль"; break;
             case 8 : month = "Август"; break;
