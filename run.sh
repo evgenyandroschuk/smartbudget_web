a4e0c29290fe702c3ee74a76ad1339d2335c0cba
evgenyandroschuk <johnstep70483@gmail.com>
2019-09-01 00:30:40 +0300

    [13] Implements Expenses by postgreSql

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -Dspring.profiles.active=prod -jar target/web-app-1.0.0.jar
--- src/main/java/com/ui/service/ExpensesService.java
+++ src/main/java/com/ui/service/ExpensesService.java
@@ -12,13 +12,14 @@ import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class ExpensesService extends AbstractService {
 
+    private final int USER_ID = 1;
+    private final String EXPENSES_TYPE = "expensesType";
     private List<Map<String, Object>> expensesTypeList;
 
     private ExpensesService(String user, String password) {
@@ -34,11 +35,11 @@ public class ExpensesService extends AbstractService {
 
         LocalDate now = LocalDate.now();
         HttpGet httpGet =
-                new HttpGet(String.format("http://localhost:7004/budget/expenses?month=%d&year=%d", now.getMonthValue(), now.getYear()));
+                new HttpGet(String.format("http://localhost:7004/budget/v2/expenses?user_id=%d&month=%d&year=%d", USER_ID, now.getMonthValue(), now.getYear()));
         authRequest(httpGet);
         HttpResponse response = client.execute(httpGet);
 
-        if (response.getStatusLine().getStatusCode()== HttpStatus.OK.value()) {
+        if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
             return getExpensesByEntity(response.getEntity());
         }
 
@@ -47,13 +48,13 @@ public class ExpensesService extends AbstractService {
     }
 
     public List<Expenses> getExpensesByYearMonth(int year, int month) throws AuthenticationException, IOException {
-        String requestPattern = "http://localhost:7004/budget/expenses?year=%d&month=%d";
-        String requestString = String.format(requestPattern, year, month);
+        String requestPattern = "http://localhost:7004/budget/v2/expenses?user_id=%d&year=%d&month=%d";
+        String requestString = String.format(requestPattern, USER_ID, year, month);
         HttpGet httpGet = new HttpGet(requestString);
         authRequest(httpGet);
         HttpResponse response = client.execute(httpGet);
 
-        if (response.getStatusLine().getStatusCode()== HttpStatus.OK.value()) {
+        if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
             return getExpensesByEntity(response.getEntity());
         }
 
@@ -65,13 +66,13 @@ public class ExpensesService extends AbstractService {
             String description, String startDate, String endDate
     ) throws AuthenticationException, IOException {
         String desc = description.replace(" ", "+");
-        String requestPattern = "http://localhost:7004/budget/expenses/description?description=%s&start=%s&end=%s";
-        String requestString = String.format(requestPattern, desc, startDate, endDate);
+        String requestPattern = "http://localhost:7004/budget/v2/expenses/description?user_id=%d&description=%s&start=%s&end=%s";
+        String requestString = String.format(requestPattern, USER_ID, desc, startDate, endDate);
         HttpGet httpGet = new HttpGet(requestString);
         authRequest(httpGet);
         HttpResponse response = client.execute(httpGet);
 
-        if (response.getStatusLine().getStatusCode()== HttpStatus.OK.value()) {
+        if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
             return getExpensesByEntity(response.getEntity());
         }
 
@@ -79,12 +80,12 @@ public class ExpensesService extends AbstractService {
         return ImmutableList.of();
     }
 
-    private List<Expenses> getExpensesByEntity (HttpEntity entity) throws IOException {
+    private List<Expenses> getExpensesByEntity(HttpEntity entity) throws IOException {
         List<Expenses> result = new LinkedList<>();
         Util.getListMapFromEntity(entity).forEach(
                 t -> {
-                    int typeId = (Integer) t.get("type");
-                    t.put("type", getTypeDescriptionById(typeId));
+                    int expensesType = (Integer) ((Map) t.get(EXPENSES_TYPE)).get("expensesTypeId");
+                    t.put("type", getTypeDescriptionById(expensesType));
                     Expenses expenses = DataConverter.convertToExpenses(t);
                     result.add(expenses);
                 }
@@ -94,12 +95,12 @@ public class ExpensesService extends AbstractService {
 
     public List<Map<String, Object>> getExpensesType() throws AuthenticationException, IOException {
         if (expensesTypeList != null) {
-            return  expensesTypeList;
+            return expensesTypeList;
         } else {
-            HttpGet httpGet = new HttpGet("http://localhost:7004/budget/expenses/type");
+            HttpGet httpGet = new HttpGet("http://localhost:7004/budget/v2/expenses/types?user_id=" + USER_ID);
             authRequest(httpGet);
             HttpResponse response = client.execute(httpGet);
-            if (response.getStatusLine().getStatusCode()== HttpStatus.OK.value()) {
+            if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
                 expensesTypeList = Util.getListMapFromEntity(response.getEntity());
                 return expensesTypeList;
             }
@@ -107,12 +108,10 @@ public class ExpensesService extends AbstractService {
         return ImmutableList.of();
     }
 
-    public String getTypeDescriptionById(Integer id) {
+    private String getTypeDescriptionById(Integer id) {
         try {
             return (String) getExpensesType().stream().filter(t -> t.get("id").equals(id)).findAny().orElse(ImmutableMap.of()).get("desc");
-        } catch (AuthenticationException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
+        } catch (AuthenticationException | IOException e) {
             throw new RuntimeException(e);
         }
     }
@@ -143,15 +142,15 @@ public class ExpensesService extends AbstractService {
         }
 
         String request = String.format(
-                "http://localhost:7004/budget/expenses/save?month=%s&year=%s&type=%s&description=%s&amount=%s",
-                month, year, type, desc.replace(" ", "+"), amount
+                "http://localhost:7004/budget/v2/expenses/save?user_id=%d&month=%s&year=%s&type=%s&description=%s&amount=%s",
+                USER_ID, month, year, type, desc.replace(" ", "+"), amount
         );
 
         HttpGet httpGet = new HttpGet(request);
         authRequest(httpGet);
         HttpResponse response = client.execute(httpGet);
 
-        if (response.getStatusLine().getStatusCode()==HttpStatus.OK.value()) {
+        if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
             return result;
         }
         throw new RuntimeException("Http status not Ok" + EntityUtils.toString(response.getEntity()));
@@ -164,11 +163,13 @@ public class ExpensesService extends AbstractService {
             return "" + amount;
         }
 
-        HttpGet httpGet = new HttpGet("http://localhost:7004/budget/expenses/update/expenses_balance?amount="+amount);
+        HttpGet httpGet = new HttpGet(
+                "http://localhost:7004/budget/v2/expenses/balance/update?amount=" + amount + "&user_id=" + USER_ID
+        );
         authRequest(httpGet);
         HttpResponse response = client.execute(httpGet);
 
-        if (response.getStatusLine().getStatusCode()==HttpStatus.OK.value()) {
+        if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
             return "" + amount;
         }
         throw new RuntimeException("Http status not Ok" + EntityUtils.toString(response.getEntity()));
@@ -187,7 +188,7 @@ public class ExpensesService extends AbstractService {
         double price = Double.parseDouble(stringPrice);
         int currency = Integer.valueOf(stringCurrency);
         String currencyValue = "Рубль";
-        if (currency == 1) currencyValue = "Доллар";
+        if (currency == USER_ID) currencyValue = "Доллар";
         if (currency == 2) currencyValue = "Евро";
 
         Map<String, String> result = ImmutableMap.of(
@@ -203,14 +204,14 @@ public class ExpensesService extends AbstractService {
 
         HttpGet httpGet = new HttpGet(
                 String.format(
-                        "http://localhost:7004/budget/fund/save?currency=%d&description=%s&price=%.2f&amount=%.2f",
-                        currency, description.replace(" ", "+"), price, amount
+                        "http://localhost:7004/budget/v2/fund/save?user_id=%d&currency=%d&description=%s&price=%.2f&amount=%.2f",
+                        USER_ID, currency, description.replace(" ", "+"), price, amount
                 )
         );
         authRequest(httpGet);
         HttpResponse response = client.execute(httpGet);
 
-        if (response.getStatusLine().getStatusCode()==HttpStatus.OK.value()) {
+        if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
             return result;
         }
         throw new RuntimeException("Http status not Ok" + EntityUtils.toString(response.getEntity()));
@@ -237,14 +238,14 @@ public class ExpensesService extends AbstractService {
 
         HttpGet httpGet = new HttpGet(
                 String.format(
-                        "http://localhost:7004/budget/currency/update?currency=%d&amount=%.2f",
-                        currency, price
+                        "http://localhost:7004/budget/v2/currency/update?user_id=%d&currency=%d&amount=%.2f",
+                        USER_ID, currency, price
                 )
         );
         authRequest(httpGet);
         HttpResponse response = client.execute(httpGet);
 
-        if (response.getStatusLine().getStatusCode()==HttpStatus.OK.value()) {
+        if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
             return result;
         }
         throw new RuntimeException("Http status not Ok" + EntityUtils.toString(response.getEntity()));
@@ -253,7 +254,8 @@ public class ExpensesService extends AbstractService {
 
     public Map<String, Object> getStatisticCurrentMonth() throws AuthenticationException, IOException {
 
-        HttpGet getExpensesAmount = new HttpGet("http://localhost:7004/budget/statistic/current");
+        HttpGet getExpensesAmount = new HttpGet(
+                "http://localhost:7004/budget/v2/statistic/current?user_id=" + USER_ID);
         authRequest(getExpensesAmount);
         HttpResponse responseTotal = client.execute(getExpensesAmount);
 
@@ -262,17 +264,19 @@ public class ExpensesService extends AbstractService {
     }
 
     private List<Map<String, Object>> getYearlyStatisticFromService(int year) throws AuthenticationException, IOException {
-        HttpGet getExpensesAmount = new HttpGet("http://localhost:7004/budget/statistic/expenses/yearly?year=" + year);
+        HttpGet getExpensesAmount = new HttpGet(
+                "http://localhost:7004/budget/v2/statistic/expenses/yearly?year=" + year + "&user_id=" + USER_ID
+        );
         authRequest(getExpensesAmount);
         HttpResponse response = client.execute(getExpensesAmount);
-        return  Util.getListMapFromEntity(response.getEntity());
+        return Util.getListMapFromEntity(response.getEntity());
     }
 
     public List<List<Object>> getYearlyMonthlyStatistic(int year) throws IOException, AuthenticationException {
         List<Map<String, Object>> origin = getYearlyStatisticFromService(year);
         List<List<Object>> statistics = new LinkedList<>();
         int m = 0;
-        while(m++ < 12) {
+        while (m++ < 12) {
             double monthlyAmount = 0;
             List<Object> monthStatistic = new LinkedList<>();
             monthStatistic.add(getMonthById(m));
@@ -281,7 +285,7 @@ public class ExpensesService extends AbstractService {
                 String typeDesc = (String) type.get("desc");
                 double amount = getMonthlyTotalByType(typeDesc, m, origin);
                 monthStatistic.add(getRoundedValue(amount));
-                if(!isIncome) {
+                if (!isIncome) {
                     monthlyAmount += amount;
                 }
             }
@@ -310,7 +314,7 @@ public class ExpensesService extends AbstractService {
 
     private double getMonthlyTotalByType(String typeDesc, int monthId, List<Map<String, Object>> origin) {
         double result = 0;
-        for(Map<String, Object> map : origin) {
+        for (Map<String, Object> map : origin) {
             String month = Integer.toString(monthId);
             if (map.get("month_id").equals(month)) {
                 if (map.get("description").equals(typeDesc)) {
@@ -325,7 +329,7 @@ public class ExpensesService extends AbstractService {
 
     private double getTotalByType(String typeDesc, List<Map<String, Object>> origin) {
         double result = 0;
-        for(Map<String, Object> map : origin) {
+        for (Map<String, Object> map : origin) {
             if (map.get("description").equals(typeDesc)) {
                 String amountString = (String) map.get("amount");
                 double amount = Double.parseDouble(amountString);
@@ -340,23 +344,47 @@ public class ExpensesService extends AbstractService {
     }
 
     private String getMonthById(int id) {
-        if (id < 1 && id > 12) {
+        if (id < USER_ID && id > 12) {
             throw new RuntimeException("Incorrect month");
         }
         String month = "";
         switch (id) {
-            case 1 : month = "Январь"; break;
-            case 2 : month = "Февраль"; break;
-            case 3 : month = "Март"; break;
-            case 4 : month = "Апрель"; break;
-            case 5 : month = "Май"; break;
-            case 6 : month = "Июнь"; break;
-            case 7 : month = "Июль"; break;
-            case 8 : month = "Август"; break;
-            case 9 : month = "Сентябрь"; break;
-            case 10 : month = "Октябрь"; break;
-            case 11 : month = "Ноябрь"; break;
-            case 12 : month = "Декабрь"; break;
+            case USER_ID:
+                month = "Январь";
+                break;
+            case 2:
+                month = "Февраль";
+                break;
+            case 3:
+                month = "Март";
+                break;
+            case 4:
+                month = "Апрель";
+                break;
+            case 5:
+                month = "Май";
+                break;
+            case 6:
+                month = "Июнь";
+                break;
+            case 7:
+                month = "Июль";
+                break;
+            case 8:
+                month = "Август";
+                break;
+            case 9:
+                month = "Сентябрь";
+                break;
+            case 10:
+                month = "Октябрь";
+                break;
+            case 11:
+                month = "Ноябрь";
+                break;
+            case 12:
+                month = "Декабрь";
+                break;
 
         }
         return month;
